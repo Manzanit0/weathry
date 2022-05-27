70df70f2d4c34c01436e34a7fc10147edbce9fbe
Manzanit0 <unico.javier@gmail.com>
2022-05-27 08:49:55 +0200

    Define CreateUser request

--- cmd/users/main.go
+++ cmd/users/main.go
@@ -7,15 +7,15 @@ import (
 
 	"google.golang.org/grpc"
 
-	authserver "github.com/manzanit0/weathry/cmd/auth/proto/gen"
+	authserver "github.com/manzanit0/weathry/cmd/users/proto/gen"
 )
 
 type server struct {
-	authserver.UnimplementedGreeterServer
+	authserver.UnimplementedUsersServer
 }
 
-func (s *server) SayHello(ctx context.Context, in *authserver.HelloRequest) (*authserver.HelloReply, error) {
-	return &authserver.HelloReply{Message: in.Name + " world"}, nil
+func (s *server) SayHello(ctx context.Context, in *authserver.CreateRequest) (*authserver.CreateResponse, error) {
+	return &authserver.CreateResponse{}, nil
 }
 
 func main() {
@@ -28,7 +28,7 @@ func main() {
 	// Create a gRPC server object
 	s := grpc.NewServer()
 	// Attach the Greeter service to the server
-	authserver.RegisterGreeterServer(s, &server{})
+	authserver.RegisterUsersServer(s, &server{})
 	// Serve gRPC Server
 	log.Println("Serving gRPC on 0.0.0.0:8080")
 	log.Fatal(s.Serve(lis))
