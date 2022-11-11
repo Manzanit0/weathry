925ab3e6bdb9cf127f39432ede7f589fc9d2660a
Manzanit0 <unico.javier@gmail.com>
2022-11-11 12:00:31 +0100

    Add mild support for edited messages

--- pkg/tgram/tgram.go
+++ pkg/tgram/tgram.go
@@ -9,8 +9,69 @@ import (
 )
 
 type WebhookRequest struct {
-	UpdateID int     `json:"update_id"`
-	Message  Message `json:"message"`
+	UpdateID      int      `json:"update_id"`
+	Message       *Message `json:"message"`
+	EditedMessage *Message `json:"edited_message"`
+}
+
+func (w WebhookRequest) GetFromUsername() string {
+	if w.Message != nil {
+		return w.Message.From.Username
+	}
+
+	if w.EditedMessage != nil {
+		return w.EditedMessage.From.Username
+	}
+
+	return ""
+}
+
+func (w WebhookRequest) GetFromID() int {
+	if w.Message != nil {
+		return w.Message.From.ID
+	}
+
+	if w.EditedMessage != nil {
+		return w.EditedMessage.From.ID
+	}
+
+	return 0
+}
+
+func (w WebhookRequest) GetFromFirstName() string {
+	if w.Message != nil {
+		return w.Message.From.FirstName
+	}
+
+	if w.EditedMessage != nil {
+		return w.EditedMessage.From.FirstName
+	}
+
+	return ""
+}
+
+func (w WebhookRequest) GetFromLastName() string {
+	if w.Message != nil {
+		return w.Message.From.LastName
+	}
+
+	if w.EditedMessage != nil {
+		return w.EditedMessage.From.LastName
+	}
+
+	return ""
+}
+
+func (w WebhookRequest) GetFromLanguageCode() string {
+	if w.Message != nil {
+		return w.Message.From.LanguageCode
+	}
+
+	if w.EditedMessage != nil {
+		return w.EditedMessage.From.LanguageCode
+	}
+
+	return ""
 }
 
 type From struct {
