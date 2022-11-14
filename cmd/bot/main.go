d0cdddc069fb36d4dfb2a93b2a8d75f605d1ceee
Manzanit0 <unico.javier@gmail.com>
2022-11-14 15:13:31 +0100

    Extract msg pkg

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -16,13 +16,13 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/manzanit0/weathry/cmd/bot/msg"
 	"github.com/manzanit0/weathry/pkg/location"
 	"github.com/manzanit0/weathry/pkg/middleware"
 	"github.com/manzanit0/weathry/pkg/pings"
 	"github.com/manzanit0/weathry/pkg/tgram"
 	"github.com/manzanit0/weathry/pkg/weather"
 	"github.com/manzanit0/weathry/pkg/whttp"
-	"github.com/olekukonko/tablewriter"
 
 	_ "github.com/jackc/pgx/v4/stdlib"
 )
@@ -181,145 +181,6 @@ func TelegramAuth(usersClient UsersClient) gin.HandlerFunc {
 	}
 }
 
-func BuildMessage(f []*weather.Forecast) string {
-	if len(f) == 0 {
-		return "hey, not sure why but I couldn't get any forecasts ¯\\_(ツ)_/¯"
-	}
-
-	// TODO: extract from here...
-	// we just want the next 3 forecasts
-	ff := make([]*weather.Forecast, 3)
-	for i := 0; i < 3; i++ {
-		ff[i] = f[i]
-	}
-
-	var sb strings.Builder
-	sb.WriteString(fmt.Sprintf("Weather Report for %s", f[0].Location))
-	for _, v := range f {
-		ts := v.FormattedDateTime()
-		sb.WriteString(fmt.Sprintf(`
-- - - - - - - - - - - - - - - - - - - - - -
-📅 %s
-🏷 %s
-🌡 %0.2f°C - %0.2f°C
-💨 %0.2f m/s
-💧 %d%%`, ts, v.Description, v.MinimumTemperature, v.MaximumTemperature, v.WindSpeed, v.Humidity))
-	}
-
-	sb.WriteString("\n- - - - - - - - - - - - - - - - - - - - - -")
-
-	return sb.String()
-}
-
-func BuildHourlyMessage(f []*weather.Forecast) string {
-	if len(f) == 0 {
-		return "hey, not sure why but I couldn't get any forecasts ¯\\_(ツ)_/¯"
-	}
-
-	// TODO: extract from here...
-	// we just want the next 9 forecasts
-	ff := make([]*weather.Forecast, 9)
-	for i := 0; i < 9; i++ {
-		ff[i] = f[i]
-	}
-
-	var sb strings.Builder
-	sb.WriteString(fmt.Sprintf("Weather Report for %s", f[0].Location))
-	for _, v := range ff {
-		ts, err := v.LocalTime("Europe/Madrid")
-		if err != nil {
-			ts = v.FormattedDateTime()
-		}
-
-		sb.WriteString(fmt.Sprintf(`
-- - - - - - - - - - - - - - - - - - - - - -
-📅 %s
-🏷 %s
-🌡 %0.2f°C
-💨 %0.2f m/s
-💧 %d%%`, ts, v.Description, v.MinimumTemperature, v.WindSpeed, v.Humidity))
-	}
-
-	sb.WriteString("\n- - - - - - - - - - - - - - - - - - - - - -")
-
-	return sb.String()
-}
-
-type messageOptions struct {
-	withTempDiff bool
-	withTime     bool
-}
-
-type MessageOption func(*messageOptions)
-
-func WithTemperatureDiff() MessageOption {
-	return func(config *messageOptions) {
-		config.withTempDiff = true
-	}
-}
-
-func WithTime() MessageOption {
-	return func(config *messageOptions) {
-		config.withTime = true
-	}
-}
-
-func BuildMessageAsTable(loc *location.Location, f []*weather.Forecast, opts ...MessageOption) string {
-	if len(f) == 0 {
-		return "hey, not sure why but I couldn't get any forecasts ¯\\_(ツ)_/¯"
-	}
-
-	options := messageOptions{withTempDiff: false, withTime: false}
-	for _, f := range opts {
-		f(&options)
-	}
-
-	b := bytes.NewBuffer([]byte{})
-	table := tablewriter.NewWriter(b)
-
-	if options.withTime {
-		table.SetHeader([]string{"Time", "Report"})
-	} else {
-
-		table.SetHeader([]string{"Date", "Report"})
-	}
-
-	for _, v := range f {
-		temp := fmt.Sprintf("%.0fºC", v.MinimumTemperature)
-		if options.withTempDiff {
-			temp = fmt.Sprintf("%.0fºC - %.0fºC", v.MinimumTemperature, v.MaximumTemperature)
-		}
-
-		dt := v.FormattedDate()
-		if options.withTime {
-			dt = v.FormattedTime()
-		}
-
-		table.Append([]string{dt, fmt.Sprintf("%s  \n%s", v.Description, temp)})
-	}
-
-	table.SetRowLine(true)
-	table.SetRowSeparator("-")
-	table.SetAutoFormatHeaders(false)
-
-	table.Render()
-
-	// we're making the assumption here that all forecasts belong to the same day.
-	if options.withTime {
-		return fmt.Sprintf("```%s  \n%s  \n%s```",
-			time.Unix(int64(f[0].DateTimeTS), 0).Format("Mon, 02 Jan 2006"),
-			loc.Name,
-			b.String(),
-		)
-
-	}
-
-	return fmt.Sprintf("```%s  \n%s```",
-		loc.Name,
-		b.String(),
-	)
-}
-
 func telegramWebhookController(locClient location.Client, weatherClient weather.Client, convos *ConvoRepository) func(c *gin.Context) {
 	return func(c *gin.Context) {
 		var p *tgram.WebhookRequest
@@ -440,7 +301,7 @@ func GetDailyWeather(locClient location.Client, weatherClient weather.Client, qu
 		return "", fmt.Errorf("get weather: %w", err)
 	}
 
-	return BuildMessageAsTable(location, forecasts, WithTemperatureDiff()), nil
+	return msg.NewForecastTableMessage(location, forecasts, msg.WithTemperatureDiff()), nil
 }
 
 func GetHourlyWeather(locClient location.Client, weatherClient weather.Client, query string) (string, error) {
@@ -460,7 +321,7 @@ func GetHourlyWeather(locClient location.Client, weatherClient weather.Client, q
 		ff[i] = forecasts[i]
 	}
 
-	return BuildMessageAsTable(location, ff, WithTime()), nil
+	return msg.NewForecastTableMessage(location, ff, msg.WithTime()), nil
 }
 
 func getQuery(text string) string {
---
+++ cmd/bot/msg/msg.go
@@ -0,0 +1,151 @@
+package msg
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/manzanit0/weathry/pkg/location"
+	"github.com/manzanit0/weathry/pkg/weather"
+	"github.com/olekukonko/tablewriter"
+)
+
+func NewEmojifiedDailyMessage(f []*weather.Forecast) string {
+	if len(f) == 0 {
+		return "hey, not sure why but I couldn't get any forecasts ¯\\_(ツ)_/¯"
+	}
+
+	// TODO: extract from here...
+	// we just want the next 3 forecasts
+	ff := make([]*weather.Forecast, 3)
+	for i := 0; i < 3; i++ {
+		ff[i] = f[i]
+	}
+
+	var sb strings.Builder
+	sb.WriteString(fmt.Sprintf("Weather Report for %s", f[0].Location))
+	for _, v := range f {
+		ts := v.FormattedDateTime()
+		sb.WriteString(fmt.Sprintf(`
+- - - - - - - - - - - - - - - - - - - - - -
+📅 %s
+🏷 %s
+🌡 %0.2f°C - %0.2f°C
+💨 %0.2f m/s
+💧 %d%%`, ts, v.Description, v.MinimumTemperature, v.MaximumTemperature, v.WindSpeed, v.Humidity))
+	}
+
+	sb.WriteString("\n- - - - - - - - - - - - - - - - - - - - - -")
+
+	return sb.String()
+}
+
+func NewEmojifiedHourlyMessage(f []*weather.Forecast) string {
+	if len(f) == 0 {
+		return "hey, not sure why but I couldn't get any forecasts ¯\\_(ツ)_/¯"
+	}
+
+	// TODO: extract from here...
+	// we just want the next 9 forecasts
+	ff := make([]*weather.Forecast, 9)
+	for i := 0; i < 9; i++ {
+		ff[i] = f[i]
+	}
+
+	var sb strings.Builder
+	sb.WriteString(fmt.Sprintf("Weather Report for %s", f[0].Location))
+	for _, v := range ff {
+		ts, err := v.LocalTime("Europe/Madrid")
+		if err != nil {
+			ts = v.FormattedDateTime()
+		}
+
+		sb.WriteString(fmt.Sprintf(`
+- - - - - - - - - - - - - - - - - - - - - -
+📅 %s
+🏷 %s
+🌡 %0.2f°C
+💨 %0.2f m/s
+💧 %d%%`, ts, v.Description, v.MinimumTemperature, v.WindSpeed, v.Humidity))
+	}
+
+	sb.WriteString("\n- - - - - - - - - - - - - - - - - - - - - -")
+
+	return sb.String()
+}
+
+type messageOptions struct {
+	withTempDiff bool
+	withTime     bool
+}
+
+type MessageOption func(*messageOptions)
+
+func WithTemperatureDiff() MessageOption {
+	return func(config *messageOptions) {
+		config.withTempDiff = true
+	}
+}
+
+func WithTime() MessageOption {
+	return func(config *messageOptions) {
+		config.withTime = true
+	}
+}
+
+func NewForecastTableMessage(loc *location.Location, f []*weather.Forecast, opts ...MessageOption) string {
+	if len(f) == 0 {
+		return "hey, not sure why but I couldn't get any forecasts ¯\\_(ツ)_/¯"
+	}
+
+	options := messageOptions{withTempDiff: false, withTime: false}
+	for _, f := range opts {
+		f(&options)
+	}
+
+	b := bytes.NewBuffer([]byte{})
+	table := tablewriter.NewWriter(b)
+
+	if options.withTime {
+		table.SetHeader([]string{"Time", "Report"})
+	} else {
+
+		table.SetHeader([]string{"Date", "Report"})
+	}
+
+	for _, v := range f {
+		temp := fmt.Sprintf("%.0fºC", v.MinimumTemperature)
+		if options.withTempDiff {
+			temp = fmt.Sprintf("%.0fºC - %.0fºC", v.MinimumTemperature, v.MaximumTemperature)
+		}
+
+		dt := v.FormattedDate()
+		if options.withTime {
+			dt = v.FormattedTime()
+		}
+
+		table.Append([]string{dt, fmt.Sprintf("%s  \n%s", v.Description, temp)})
+	}
+
+	table.SetRowLine(true)
+	table.SetRowSeparator("-")
+	table.SetAutoFormatHeaders(false)
+
+	table.Render()
+
+	// we're making the assumption here that all forecasts belong to the same day.
+	if options.withTime {
+		return fmt.Sprintf("```%s  \n%s  \n%s```",
+			time.Unix(int64(f[0].DateTimeTS), 0).Format("Mon, 02 Jan 2006"),
+			loc.Name,
+			b.String(),
+		)
+
+	}
+
+	return fmt.Sprintf("```%s  \n%s```",
+		loc.Name,
+		b.String(),
+	)
+}
