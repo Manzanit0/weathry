f31e409d85ec6488de89f75c7b137f91f3f8b8e2
Javier García <unico.javier@gmail.com>
2023-09-05 18:58:33 +0200

    escape

--- cmd/bot/api/messages.go
+++ cmd/bot/api/messages.go
@@ -115,7 +115,7 @@ func (g *MessageController) setHome(ctx context.Context, p *tgram.WebhookRequest
 		return msg.MsgUnableToGetReport
 	}
 
-	return fmt.Sprintf(`Successfully set %s as your home! I'll now watch it for any weather changes and let you know 🙂`, locationName)
+	return fmt.Sprintf(`Successfully set %s as your home! I\\'ll now watch it for any weather changes and let you know 🙂`, locationName)
 }
 
 func (g *MessageController) ProcessNonCommand(ctx context.Context, p *tgram.WebhookRequest) string {
